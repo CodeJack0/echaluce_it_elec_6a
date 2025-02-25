37b9da8a7751c2121806ca291fd946b80456a07f
diobrando00122@gmail.com <diobrando00122@gmail.com>
2025-02-25 16:13:57 +0800

    5th/2 commit

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require ('express');
 
 const app = express();
-const bodyParser =require('body-parser')
+const bodyParser =require('body-parser');
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
